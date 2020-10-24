f870250a1c8b11d9b763ec23d4b58c305da97121
OPPANLENDER Gabriel <gabishowytb@gmail.com>
2020-10-24 15:36:52 +0200

    blblblbl

---
+++ the witch land/Assets/CameraRotation.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    [SerializeField]
+    private Vector3 rotation;
+    [SerializeField]
+    private Vector3 cameraRotation;
+
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private Vector3 velocity;
+    [SerializeField]
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+        Cursor.visible = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    void PerformMovement() //si l'on met juste void ou private void c pareil
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        cam.transform.Rotate(-cameraRotation);
+
+    }
+
+    void Update()
+    {
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0, _yRot, 0) * lookSensitivity;
+
+        Rotate(_rotation);
+
+        //on va calculer la rotation de la camera en un vecteur 3d
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * lookSensitivity;
+
+        RotateCamera(_cameraRotation);
+        PerformRotation();
+
+
+    }
+
+
+}
---
+++ the witch land/Assets/playerMotor.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMotor : MonoBehaviour
+{
+    public float speed = 3;
+    public float walkSpeed = 9;
+
+    public Vector3 jumpSpeed;
+    CapsuleCollider playerCollider;
+
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    private Vector3 rotation;
+    private Vector3 cameraRotation;
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+
+
+    void Start()
+    {
+        playerCollider = gameObject.GetComponent<CapsuleCollider>(); //réferencer playerCollider en temps que components
+    }
+
+
+    [SerializeField] LayerMask playerLayer;
+
+    bool IsGrounded()
+    {
+        return Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y - 0.1f, playerCollider.bounds.center.z), 0.1f, playerLayer); //Verifie si notre personnage est au sol
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && !(Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(walkSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q) && !(Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q) && (Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(-walkSpeed * Time.deltaTime, 0, 0);
+        }
+
+        //avancer reculer
+
+        if (Input.GetKey(KeyCode.Z) && (Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(0, 0, walkSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Z) && !(Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) && (Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(0, 0, -walkSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) && !(Input.GetKey(KeyCode.X)))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) //le return s'occupe lui meme de dire si c'est vrai ou faux.
+        {
+            // preparation du saut (necessaire)
+            Vector3 v = gameObject.GetComponent<Rigidbody>().velocity;
+            v.y = jumpSpeed.y;
+
+            //saut
+
+            gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
+        }
+    }
+}
